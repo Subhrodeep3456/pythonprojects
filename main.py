c3ca789c9debf29e5a302c153cd26069faa9a52d
cometcore <75169187+Subhrodeep3456@users.noreply.github.com>
2024-08-22 19:19:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #Airline company
 import csv
 import pickle 
+# import mysql.connector
 
 
 print("Trans Oceanic Airlines")
@@ -85,130 +86,79 @@ while ans == 'yes':
                 else:
                     print("Found")
                 f.close()
-            
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ans = input("Continue (yes/no) : ")
+        def modify():
+            import pickle
+            found = False
+            f = open('customer.dat','rb')
+            g = open('customer1.dat','wb')
+            s = input("Enter customer name : ")
+            try:
+                while True:
+                    p = pickle.load(f)
+                    if p['Customer Name'] == s:
+                        print("Customer found")
+                        p['Age'] = int(input("Enter new age : "))
+                        p['Gender'] = input("Enter new gender : ")
+                        p['Class'] = input("Enter new class : ")
+                        pickle.dump(p,g)
+                        found = True
+            except EOFError:
+                if found == False:
+                    print("No records found")
+                else:
+                    print("Modified")
+                f.close()
+                g.close()
 
 
-        
+        def delete():
+            import pickle
+            found = False
+            f = open('customer.dat','rb')
+            g = open('customer1.dat','wb')
+            s = input("Enter customer name : ")
+            try:
+                while True:
+                    p = pickle.load(f)
+                    if p['Customer Name'] == s:
+                        print("Customer found")
+                        found = True
+                    else:
+                        pickle.dump(p,g)
+            except EOFError:
+                if found == False:
+                    print("No records found")
+                else:
+                    print("Deleted")
+                f.close()
+                g.close()
 
+        answer = 'y'
+        while answer == 'y':
+            print("1 to view customer details")
+            print("2 to add customer details")
+            print("3 to search customer details")
+            print("4 to modify customer details")
+            print("5 to delete customer details")
+            print("6 to exit")
+            answer = input("Enter your choice : ")
+            if answer == '1':
+                redreport()
+            elif answer == '2':
+                add()
+            elif answer == '3':
+                search()
+            elif answer == '4':
+                modify()
+            elif answer == '5':
+                delete()
+            elif answer == '6':
+                break
 
-
-
-
-
+    # elif p == 2:
+    #     print("Seating")
+    #     import csv
+    #     with open('seating.csv') as cf:
 
 
 
